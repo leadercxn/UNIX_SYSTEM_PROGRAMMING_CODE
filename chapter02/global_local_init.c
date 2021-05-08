120695be7d58e8c0f39217b878eae563aa74f6bf
陈小念 <sensoro@sensorodeMacBook-Pro.local>
2021-05-08 16:45:57 +0800

    新增:  书中的代码

---
+++ chapter02/global_local_init.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int arg1;
+int arg2 = 0;
+
+int main(void)
+{
+    int arg3;
+    int arg4 = 0;
+
+    printf("arg1 = %d,arg2 = %d,arg3 = %d,arg4 = %d\n",arg1,arg2,arg3,arg4);
+}
